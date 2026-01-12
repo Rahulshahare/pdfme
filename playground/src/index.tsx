de313c264ba7a02a9a76eab8a5f589b32065f812
Rahulshahare <rahulshahare@gmail.com>
2026-01-12 20:07:59 +0530

    basename added for better Routing on server

--- playground/src/index.tsx
+++ playground/src/index.tsx
@@ -16,7 +16,7 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/pdfme">
       <App />
     </BrowserRouter>
   </React.StrictMode>
